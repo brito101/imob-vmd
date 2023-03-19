9211a2b65f95baf2eeb83831c3b608e64a4d753e
brito101 <rodrigobrito101@hotmail.com>
2023-03-19 11:46:18 -0300

    Edit contact_success page

--- resources/views/web/contact_success.blade.php
+++ resources/views/web/contact_success.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 
 <div class="container p-5">
-    <h2 class="text-center text-support">Seu e-mail foi enviado com sucesso!<br>Em breve entraremos em contato.</h2>
+    <h2 class="text-center text-front">Seu e-mail foi enviado com sucesso!<br>Em breve entraremos em contato.</h2>
     <div class="d-flex justify-content-center pt-4">
         <div class="text-center btn-custom w-md-25">
-            <a href="{{ url()->previous() }}" class="text-opposit text-decoration-none">... Continuar navegando!</a>
+            <a href="{{ url()->previous() }}" class="text-opposit text-decoration-none text-front">... Continuar navegando!</a>
         </div>
     </div>
 </div>
